a25fdcc3def41cf21f15817d10a54baa5cb1104d
agallardoysla <agallardoysla@gmail.com>
2020-09-17 20:25:02 -0500

    container

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-//importaciones necesarias para redux
+//importaciones necesarias para redux//
 import { connect } from 'react-redux';
 import { actions } from './redux';
 import { getAllUsers } from './apis/firebase';
