807cfeedf3a56e69b80e4571da87fc17d767eeb4
agallardoysla <agallardoysla@gmail.com>
2020-11-11 10:46:12 -0500

    pre firebase wifi

--- App.js
+++ App.js
@@ -1,95 +1,97 @@
-import React, { useEffect, useRef, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useRef, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import * as Font from "expo-font";
-import Constants from "expo-constants";
+import * as Font from 'expo-font';
+import Constants from 'expo-constants';
 
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist"; //NO BORRAR
-import * as Notifications from "expo-notifications";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist'; //NO BORRAR
+import * as Notifications from 'expo-notifications';
 
-import { store, persistor } from "./src/redux/store";
-import Cargando from "./src/generales/Cargando";
-import Navigate from "./src/Navigate";
-import { colores } from "./src/constantes/Temas";
-import { StatusBar } from "expo-status-bar";
+import { store, persistor } from './src/redux/store';
+import Cargando from './src/generales/Cargando';
+import Navigate from './src/Navigate';
+import { colores } from './src/constantes/Temas';
+import { StatusBar } from 'expo-status-bar';
+
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+import { firebaseApp } from './src/apis/firebase';
 
 Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false,
+	}),
 });
 
 function App() {
-  const [load, setload] = useState(true);
+	const [load, setload] = useState(true);
 
-  const notificationListener = useRef();
-  const responseListener = useRef();
+	const notificationListener = useRef();
+	const responseListener = useRef();
 
-  useEffect(() => {
-    // This listener is fired whenever a notification is received while the app is foregrounded
-    notificationListener.current = Notifications.addNotificationReceivedListener(
-      (notification) => {
-        console.log(notification);
-      }
-    );
+	useEffect(() => {
+		// This listener is fired whenever a notification is received while the app is foregrounded
+		notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+			console.log(notification);
+		});
 
-    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(
-      (response) => {
-        console.log(response);
-      }
-    );
+		// This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+		responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+			console.log(response);
+		});
 
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener);
-      Notifications.removeNotificationSubscription(responseListener);
-    };
-  }, []);
+		return () => {
+			Notifications.removeNotificationSubscription(notificationListener);
+			Notifications.removeNotificationSubscription(responseListener);
+		};
+	}, []);
 
-  const renderLoading = () => (
-    <View style={styles.container}>
-      <Cargando />
-    </View>
-  );
+	const renderLoading = () => (
+		<View style={styles.container}>
+			<Cargando />
+		</View>
+	);
 
-  const fonts = async () => {
-    setload(true);
-    await Font.loadAsync({
-      RobotoBold: require("./assets/fonts/Roboto-BoldCondensed.ttf"),
-      RobotoRegular: require("./assets/fonts/Roboto-Condensed.ttf"),
-      RobotoItalic: require("./assets/fonts/Roboto-CondensedItalic.ttf"),
-    });
-    setload(false);
-  };
+	const fonts = async () => {
+		setload(true);
+		await Font.loadAsync({
+			RobotoBold: require('./assets/fonts/Roboto-BoldCondensed.ttf'),
+			RobotoRegular: require('./assets/fonts/Roboto-Condensed.ttf'),
+			RobotoItalic: require('./assets/fonts/Roboto-CondensedItalic.ttf'),
+		});
+		setload(false);
+	};
 
-  useEffect(() => {
-    // persistStore(store).purge();
+	useEffect(() => {
+		const firestore = firebase.firestore(firebaseApp);
+		// persistStore(store).purge();
 
-    fonts();
-  }, []);
+		fonts();
+	}, []);
 
-  return (
-    !load && (
-      <Provider store={store}>
-        <PersistGate persistor={persistor} loading={renderLoading()}>
-          <Navigate />
-          <StatusBar style='light' backgroundColor={colores.bgOscuro} />
-        </PersistGate>
-      </Provider>
-    )
-  );
+	return (
+		!load && (
+			<Provider store={store}>
+				<PersistGate persistor={persistor} loading={renderLoading()}>
+					<Navigate />
+					<StatusBar style="light" backgroundColor={colores.bgOscuro} />
+				</PersistGate>
+			</Provider>
+		)
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    paddingTop: Platform.OS == "ios" ? 0 : Constants.statusBarHeight,
-  },
+	container: {
+		flex: 1,
+		width: '100%',
+		paddingTop: Platform.OS == 'ios' ? 0 : Constants.statusBarHeight,
+	},
 });
 
 export default App;
--- src/screens/Auth/PantallaLogin.js
+++ src/screens/Auth/PantallaLogin.js
@@ -1,306 +1,288 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  TouchableNativeFeedback,
-  TextInput,
-  Platform,
-  StatusBar,
-  Alert,
-} from "react-native";
-import { RFPercentage } from "react-native-responsive-fontsize";
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	TouchableNativeFeedback,
+	TextInput,
+	Platform,
+	StatusBar,
+	Alert,
+} from 'react-native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
 
-import ElGasLogo from "../../components/Icons/ElGasLogo";
-import EmailIcon from "../../components/Icons/EmailIcon";
-import FbIcon from "../../components/Icons/FbIcon";
-import LockIcon from "../../components/Icons/LockIcon";
-import { colores, pantalla } from "../../constantes/Temas";
-import Container from "../../generales/Container";
-import useForm from "../../hooks/useForm";
-import { ValidateForm } from "../../functions/ValidateForm";
-import { getCollection, logIn } from "../../apis/querys";
-import { connect, useDispatch } from "react-redux";
-import { actions } from "../../redux/index";
-import { facebookLogIn } from "../../apis/AccountsLogin";
+import ElGasLogo from '../../components/Icons/ElGasLogo';
+import EmailIcon from '../../components/Icons/EmailIcon';
+import FbIcon from '../../components/Icons/FbIcon';
+import LockIcon from '../../components/Icons/LockIcon';
+import { colores, pantalla } from '../../constantes/Temas';
+import Container from '../../generales/Container';
+import useForm from '../../hooks/useForm';
+import { ValidateForm } from '../../functions/ValidateForm';
+import { getCollection, logIn } from '../../apis/querys';
+import { connect, useDispatch } from 'react-redux';
+import { actions } from '../../redux/index';
+import { facebookLogIn } from '../../apis/AccountsLogin';
 
 const initialValues = {
-  email: "",
-  password: "",
+	email: '',
+	password: '',
 };
 
 const PantallaLogin = (props) => {
-  const form = useForm({ initialValues });
-  const dispatch = useDispatch();
-  // console.log(form);
-  const [loginResponse, setLoginResponse] = useState(null);
+	const form = useForm({ initialValues });
+	const dispatch = useDispatch();
+	// console.log(form);
+	const [loginResponse, setLoginResponse] = useState(null);
 
-  const fbLogin = async () => {
-    const response = await facebookLogIn();
-    if (response) {
-      console.log("Resp", response);
-      dispatch(actions.actualizarLogin(response.user));
-    }
-    console.log("Facebook", response);
-  };
+	const fbLogin = async () => {
+		const response = await facebookLogIn();
+		if (response) {
+			console.log('Resp', response);
+			dispatch(actions.actualizarLogin(response.user));
+		}
+		console.log('Facebook', response);
+	};
 
-  const onLogin = () => {
-    const { email, password } = form.fields;
+	const onLogin = () => {
+		const { email, password } = form.fields;
 
-    if (ValidateForm(form)) {
-      logIn(email, password).then(async (x) => {
-        console.log("Press", x);
+		if (ValidateForm(form)) {
+			logIn(email, password).then(async (x) => {
+				console.log('Press', x);
 
-        if (x.type !== "error") {
-          dispatch(actions.actualizarLogin({ ...x.value, isLogged: true }));
+				if (x.type !== 'error') {
+					dispatch(actions.actualizarLogin({ ...x.value, isLogged: true }));
 
-          // Fetching logged user from 'Usuarios' collection
-          const userData = getCollection("plant_usuarios").then(
-            (response) => response
-          );
+					// Fetching logged user from 'Usuarios' collection
+					const userData = getCollection('plant_usuarios').then((response) => response);
 
-          const usersResponse = await userData;
+					const usersResponse = await userData;
 
-          const foundUser = usersResponse.find((us) => us.uid === x.value.uid);
+					const foundUser = usersResponse.find((us) => us.uid === x.value.uid);
 
-          dispatch(actions.setUser(foundUser));
-        }
-      });
-    } else {
-      Alert.alert("Todos los campos son obligatorios");
-    }
-  };
-  useEffect(() => {
-    console.log(form, loginResponse);
-    if (loginResponse) {
-      if (loginResponse.type === "success") {
-        props.dispatch(
-          actions.actualizarLogin({ ...loginResponse.value, isLogged: true })
-        );
-      }
-    }
-  }, [loginResponse]);
+					dispatch(actions.setUser(foundUser));
+				}
+			});
+		} else {
+			Alert.alert('Todos los campos son obligatorios');
+		}
+	};
+	useEffect(() => {
+		console.log(form, loginResponse);
+		if (loginResponse) {
+			if (loginResponse.type === 'success') {
+				props.dispatch(actions.actualizarLogin({ ...loginResponse.value, isLogged: true }));
+			}
+		}
+	}, [loginResponse]);
 
-  return (
-    <Container styleContainer={styles.screen} footer={false}>
-      <View
-        style={[
-          styles.logo,
-          { marginTop: pantalla.screenHeight <= 592 ? 50 : 125 },
-        ]}
-      >
-        <ElGasLogo height="100%" width="100%" />
-      </View>
-      <Text style={styles.textoBienvenido}>Bienvenido</Text>
+	return (
+		<Container styleContainer={styles.screen} footer={false}>
+			<View style={[styles.logo, { marginTop: pantalla.screenHeight <= 592 ? 50 : 125 }]}>
+				<ElGasLogo height="100%" width="100%" />
+			</View>
+			<Text style={styles.textoBienvenido}>Bienvenido</Text>
 
-      <View style={styles.controles}>
-        <View
-          style={{
-            height: 60,
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <View
-            style={{
-              height: 14,
-              width: 25,
-              justifyContent: "center",
-            }}
-          >
-            <EmailIcon width="100%" height="100%" color={colores.amarillo} />
-          </View>
-          <TextInput
-            style={[
-              {
-                width: "80%",
-                height: "60%",
-                color: "#FFFFFF",
-                borderBottomColor: "#FFFFFF",
-                borderBottomWidth: 1,
-                marginLeft: 5,
-                marginBottom: 5,
-                fontSize: RFPercentage(2.2),
-              },
-            ]}
-            placeholder="E-mail"
-            keyboardType="email-address"
-            {...form.getInput("email")}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            height: 60,
-          }}
-        >
-          <View
-            style={{
-              height: 20,
-              width: 20,
-              justifyContent: "center",
-            }}
-          >
-            <LockIcon height="100%" width="100%" />
-          </View>
-          <TextInput
-            style={[
-              {
-                width: "80%",
-                height: "60%",
-                color: "#FFFFFF",
-                borderBottomColor: "#FFFFFF",
-                borderBottomWidth: 1,
-                marginLeft: 5,
-                fontSize: RFPercentage(2.2),
-              },
-            ]}
-            placeholder="Password"
-            secureTextEntry
-            {...form.getInput("password")}
-          />
-        </View>
-      </View>
+			<View style={styles.controles}>
+				<View
+					style={{
+						height: 60,
+						flexDirection: 'row',
+						alignItems: 'center',
+					}}
+				>
+					<View
+						style={{
+							height: 14,
+							width: 25,
+							justifyContent: 'center',
+						}}
+					>
+						<EmailIcon width="100%" height="100%" color={colores.amarillo} />
+					</View>
+					<TextInput
+						style={[
+							{
+								width: '80%',
+								height: '60%',
+								color: '#FFFFFF',
+								borderBottomColor: '#FFFFFF',
+								borderBottomWidth: 1,
+								marginLeft: 5,
+								marginBottom: 5,
+								fontSize: RFPercentage(2.2),
+							},
+						]}
+						placeholder="E-mail"
+						keyboardType="email-address"
+						{...form.getInput('email')}
+					/>
+				</View>
+				<View
+					style={{
+						flexDirection: 'row',
+						alignItems: 'center',
+						height: 60,
+					}}
+				>
+					<View
+						style={{
+							height: 20,
+							width: 20,
+							justifyContent: 'center',
+						}}
+					>
+						<LockIcon height="100%" width="100%" />
+					</View>
+					<TextInput
+						style={[
+							{
+								width: '80%',
+								height: '60%',
+								color: '#FFFFFF',
+								borderBottomColor: '#FFFFFF',
+								borderBottomWidth: 1,
+								marginLeft: 5,
+								fontSize: RFPercentage(2.2),
+							},
+						]}
+						placeholder="Password"
+						secureTextEntry
+						{...form.getInput('password')}
+					/>
+				</View>
+			</View>
 
-      <View style={styles.contenedor}>
-        <Button color="#fff" style={{ marginBottom: 10 }} onPress={onLogin}>
-          <Text style={{ textTransform: "uppercase", fontWeight: "bold" }}>
-            Ingresar
-          </Text>
-        </Button>
-        <Button color="#227BC4" horizontal onPress={fbLogin}>
-          <View
-            style={{
-              width: "10%",
-              height: "100%",
-              marginLeft: 25,
-              justifyContent: "center",
-            }}
-          >
-            <FbIcon height={"35%"} width={"35%"} />
-          </View>
-          <View style={{ width: "90%", marginLeft: 5 }}>
-            <Text style={{ fontSize: RFPercentage(2.2), color: "#fff" }}>
-              Continuar con Facebook
-            </Text>
-          </View>
-        </Button>
+			<View style={styles.contenedor}>
+				<Button color="#fff" style={{ marginBottom: 10 }} onPress={onLogin}>
+					<Text style={{ textTransform: 'uppercase', fontWeight: 'bold' }}>Ingresar</Text>
+				</Button>
+				<Button color="#227BC4" horizontal onPress={fbLogin}>
+					<View
+						style={{
+							width: '10%',
+							height: '100%',
+							marginLeft: 25,
+							justifyContent: 'center',
+						}}
+					>
+						<FbIcon height={'35%'} width={'35%'} />
+					</View>
+					<View style={{ width: '90%', marginLeft: 5 }}>
+						<Text style={{ fontSize: RFPercentage(2.2), color: '#fff' }}>Continuar con Facebook</Text>
+					</View>
+				</Button>
 
-        <View style={{ alignItems: "center", marginVertical: 10 }}>
-          <TouchableOpacity
-            activeOpacity={0.6}
-            style={{ marginVertical: 10 }}
-            onPress={() => props.navigation.navigate("Registrarse")}
-          >
-            <Text style={{ color: "#fff" }}>
-              ¿No tienes una cuenta? Créala aquí
-            </Text>
-          </TouchableOpacity>
+				<View style={{ alignItems: 'center', marginVertical: 10 }}>
+					<TouchableOpacity
+						activeOpacity={0.6}
+						style={{ marginVertical: 10 }}
+						onPress={() => props.navigation.navigate('Registrarse')}
+					>
+						<Text style={{ color: '#fff' }}>¿No tienes una cuenta? Créala aquí</Text>
+					</TouchableOpacity>
 
-          <TouchableOpacity
-            activeOpacity={0.6}
-            onPress={() => props.navigation.navigate("RecuperarContrasena")}
-          >
-            <Text style={{ color: "#fff" }}>Olvidé mi contraseña</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+					<TouchableOpacity
+						activeOpacity={0.6}
+						onPress={() => props.navigation.navigate('RecuperarContrasena')}
+					>
+						<Text style={{ color: '#fff' }}>Olvidé mi contraseña</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
 
-      <StatusBar barStyle="light-content" />
-    </Container>
-  );
+			<StatusBar barStyle="light-content" />
+		</Container>
+	);
 };
 
 const Button = (props) => {
-  let TouchableComponent = TouchableOpacity;
-  //const {onPress=()=>{}}= props
+	let TouchableComponent = TouchableOpacity;
+	//const {onPress=()=>{}}= props
 
-  if (Platform.OS === "android" && Platform.Version >= 21) {
-    TouchableComponent = TouchableNativeFeedback;
-  }
+	if (Platform.OS === 'android' && Platform.Version >= 21) {
+		TouchableComponent = TouchableNativeFeedback;
+	}
 
-  return (
-    <View
-      style={[
-        {
-          height: 40,
-          width: "75%",
-          borderRadius: 30,
-          borderWidth: 1,
-          overflow:
-            Platform.OS === "android" && Platform.Version >= 21
-              ? "hidden"
-              : "visible",
-          backgroundColor: props.color,
-          borderColor: "transparent",
-        },
-        props.style,
-      ]}
-    >
-      <TouchableComponent
-        onPress={() => {
-          props.onPress();
-        }}
-        activeOpacity={0.6}
-      >
-        <View
-          style={
-            !props.horizontal
-              ? {
-                  flex: 1,
-                  height: "100%",
-                  width: "100%",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }
-              : {
-                  flex: 1,
-                  height: "100%",
-                  width: "100%",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }
-          }
-        >
-          {props.children}
-        </View>
-      </TouchableComponent>
-    </View>
-  );
+	return (
+		<View
+			style={[
+				{
+					height: 40,
+					width: '75%',
+					borderRadius: 30,
+					borderWidth: 1,
+					overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible',
+					backgroundColor: props.color,
+					borderColor: 'transparent',
+				},
+				props.style,
+			]}
+		>
+			<TouchableComponent
+				onPress={() => {
+					props.onPress();
+				}}
+				activeOpacity={0.6}
+			>
+				<View
+					style={
+						!props.horizontal
+							? {
+									flex: 1,
+									height: '100%',
+									width: '100%',
+									alignItems: 'center',
+									justifyContent: 'center',
+							  }
+							: {
+									flex: 1,
+									height: '100%',
+									width: '100%',
+									flexDirection: 'row',
+									alignItems: 'center',
+									justifyContent: 'space-between',
+							  }
+					}
+				>
+					{props.children}
+				</View>
+			</TouchableComponent>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    height: "100%",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: colores.bgOscuro,
-  },
-  logo: {
-    width: "100%",
-    height: "20%",
-    marginTop: 50,
-  },
-  textoBienvenido: {
-    fontSize: RFPercentage(3),
-    color: "#fff",
-  },
-  controles: {
-    width: "90%",
-    height: "25%",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 15,
-    marginBottom: 20,
-  },
+	screen: {
+		height: '100%',
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		backgroundColor: colores.bgOscuro,
+	},
+	logo: {
+		width: '100%',
+		height: '20%',
+		marginTop: 50,
+	},
+	textoBienvenido: {
+		fontSize: RFPercentage(3),
+		color: '#fff',
+	},
+	controles: {
+		width: '90%',
+		height: '25%',
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginTop: 15,
+		marginBottom: 20,
+	},
 
-  contenedor: {
-    height: "30%",
-    width: "90%",
-    alignItems: "center",
-  },
+	contenedor: {
+		height: '30%',
+		width: '90%',
+		alignItems: 'center',
+	},
 });
 export default connect()(PantallaLogin);
